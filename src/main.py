370e89c13c0c9348249e65e6e6c9bc0170126552
manhtuanthusinh <tuanlaimanh20041@gmail.com>
2025-10-16 09:50:14 +0700

    main executing mongodb client

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
+from databases.mongodb_connect import mongodb_connect
 from databases.mysql_connect import MYSQL_connect
 from config.database_config import get_db_config
 
 def main(config):
-    with MYSQL_connect(config["mysql"].host, config["mysql"].port, config["mysql"].user, config["mysql"].password) as mysql_client:
+    config_mysql = config["mysql"]
+    with MYSQL_connect(config_mysql.host, config_mysql.port, config_mysql.user, config_mysql.password) as mysql_client:
         mysql_client.connect()
+
+    config_mongodb = config["mongodb"]
+    with mongodb_connect(config_mongodb.uri, config_mongodb.databases) as mongodb_client:
+        mongodb_client.connect()
 
 if __name__ == "__main__":
     config = get_db_config()
